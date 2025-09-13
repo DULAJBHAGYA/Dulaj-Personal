6a432acdc8904add7858054eeabdac7f8e1d12dd
Dulaj Bhagya Upananda <95066839+DULAJBHAGYA@users.noreply.github.com>
2025-09-13 11:29:10 +0530

    update build errors

--- personal-portfolio/src/app/about/page.tsx
+++ personal-portfolio/src/app/about/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/Navbar";
 import About from "@/components/About";
-import Skills from "@/components/Skills";
 import Footer from "@/components/Footer";
 import { Metadata } from 'next';
 
